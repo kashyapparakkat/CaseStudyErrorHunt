05516c98aea6e55596ac6838e697ace080d4fea3
kashyap <kashyap.p@myntra.com>
2022-08-10 09:00:53 +0530

    Added atlas mongo cred

--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Library');
+//mongoose.connect('mongodb://localhost:27017/Library');
+const PASSWORD = encodeURIComponent('User@123456');
+mongoose.connect('mongodb+srv://user:User%2540123456@cluster0.cm452yw.mongodb.net/Library?retryWrites=true&w=majority');
 const Schema = mongoose.Schema;
 
 
